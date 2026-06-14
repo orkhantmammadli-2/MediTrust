a1165e7a055a1d5683ab7587b1ea310f5f776d50
OrkhanTMammadli <orkhantmammadli@gmail.com>
2026-06-15 02:42:13 +0400

    15 june

---
+++ aiService/src/test/java/com/ltc/aiservice/AiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltc.aiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
